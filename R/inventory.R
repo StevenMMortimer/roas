ab114a6ab21a498f7fc852cbbb9ab51c42da23cb
Steven Mortimer <steven.mortimer@dominionenterprises.com>
2015-10-17 23:08:11 -0400

    Add Zone and Geo Inventory Functionality

--- R/inventory.R
+++ R/inventory.R
@@ -96,7 +96,7 @@ basic_inventory_request <- function(credentials,
   keywords_type <- inventory_reports[which_report_row, 'keywords_type']
   report_id <- inventory_reports[which_report_row, 'report_id']
   if(length(report_id)!=1){
-    stop('report_id not found')
+    stop('report_id not found, please refer to dataset inventory_reports for correct report_type and report_name')
   }
   
   adxml_node <- newXMLNode("AdXML")
@@ -223,12 +223,12 @@ search_inventory_request <- function(credentials,
                                                    'KeywordStatistics', 
                                                    'KeywordBooked'), 
                                      report_name,
+                                     max_row="100",
                                      keywords=NULL,
                                      position=NULL,
                                      campaign_id=NULL,
                                      site_domain=NULL,
                                      section_id=NULL,
-                                     id=NULL,
                                      start_date=NULL,
                                      end_date=NULL){
   
@@ -243,7 +243,7 @@ search_inventory_request <- function(credentials,
                          (inventory_reports$report_name==tolower(report_name)))
   report_id <- inventory_reports[which_report_row, 'report_id']
   if(length(report_id)!=1){
-    stop('report_id not found')
+    stop('report_id not found, please refer to dataset inventory_reports for correct report_type and report_name')
   }
   
   adxml_node <- newXMLNode("AdXML")
@@ -264,18 +264,18 @@ search_inventory_request <- function(credentials,
     }
   }
   if (!is.null(campaign_id)){
-    for (p in position){
-      position_node <- newXMLNode("CampaignIds", p, parent = report_node)
+    for (c in campaign_id){
+      position_node <- newXMLNode("CampaignIds", c, parent = report_node)
     }
   }
   if (!is.null(site_domain)){
-    for (p in position){
-      position_node <- newXMLNode("SiteDomain", p, parent = report_node)
+    for (s in site_domain){
+      position_node <- newXMLNode("SiteDomain", s, parent = report_node)
     }
   }
   if (!is.null(section_id)){
-    for (p in position){
-      position_node <- newXMLNode("SectionId", p, parent = report_node)
+    for (e in section_id){
+      position_node <- newXMLNode("SectionId", e, parent = report_node)
     }
   }
   if (!is.null(start_date)){
@@ -298,8 +298,241 @@ search_inventory_request <- function(credentials,
   return(parsed_result)
 }
 
-#geo_inventory_request
-#zone_inventory_request
+#' Retrieve Geo Inventory Reports
+#' 
+#' This function returns a data.frame of inventory stats 
+#' and forecasts broken down by a specified geography
+#'
+#' @usage geo_inventory_request(credentials, 
+#'                                 report_type=c('Site', 
+#'                                               'Section'), 
+#'                                 report_geo=c('Executive Summary', 'Continent', 
+#'                                              'Country', 'State', 
+#'                                              'City', 'Postal Code', 
+#'                                              'Telephone Area Code', 
+#'                                              'DMA', 'MSA', 'Bandwidth'),
+#'                                 report_outlook=c('Statistics', 'Forecast'),
+#'                                 id,
+#'                                 max_row="100",
+#'                                 position=NULL,
+#'                                 start_date=NULL,
+#'                                 end_date=NULL)
+#' @concept api inventory geo report
+#' @include utils.R data.R
+#' @param credentials a character string as returned by \link{build_credentials}
+#' @param report_type a character string either "Site" or "Section" as these
+#' are the only types supported for geo inventory reports
+#' @param report_geo a character string from the list of options. "Summary" returns an
+#' executive summary, while the rest provide a specific geographic breakdown.
+#' @param report_outlook a character string specifying the report to be a descriptive
+#' view or a forecast. Note that forecasts must have start and end dates greater than 
+#' the current date, otherwise they will return 0 row data.frames.
+#' @param id a character string containing an identifying name for a Site or Section
+#' depending on the report_type
+#' @param max_row a character integer limiting the number of rows returned. 
+#' A character is recommended to avoid larger numbers being formatted
+#' in scientific notation and not being properly interpreted by the API.
+#' @param position a character vector containing position names. 
+#' Multiple "position" elements can be specified at once, hence a vector is accepted
+#' instead of a string
+#' @param start_date a character string for inquiring inventory detail report. 
+#' This field is optional and must be in the yyyy-mm-dd format.
+#' @param end_date a character string for inquiring the inventory detail report by 
+#' given schedule. This field is optional and must be in the yyyy-mm-dd format.
+#' @return A \code{data.frame} of inventory data in the format of the specified
+#' report_type and report_name
+#' @note Search inventory requests are only available if the Search module is enabled on the account
+#' @note The arguments for position, campaign_id, site_domain, section_id are optional, but helpful 
+#' in narrowing the focus for the resultset to shorten request and parsing time. 
+#' @examples
+#' \dontrun{
+#' my_credentials <- build_credentials('myaccount', 
+#'                                     'myusername', 
+#'                                     'mypassword')
+#'                                     
+#' site_geo <- geo_inventory_request(credentials=my_credentials, 
+#'                                   report_type='Site', 
+#'                                   report_geo='State',
+#'                                   report_outlook='Statistics'
+#'                                   id='www.mysite.com',
+#'                                   start_date='2015-09-01', 
+#'                                   end_date='2015-09-30')
+#'                                   
+#' # Note that forecast reports must have start and end dates >= Sys.Date()
+#' otherwise they will return 0 row data.frames                                  
+#' section_geo <- geo_inventory_request(credentials=my_credentials, 
+#'                                      report_type='Section', 
+#'                                      report_geo='DMA',
+#'                                      report_outlook='Forecast',
+#'                                      id='Books',
+#'                                      max_row="20000",
+#'                                      position=c('Bottom', 'Bottom1'),
+#'                                      start_date='2015-12-01', 
+#'                                      end_date='2015-12-31')
+#'                                      
+#' }
+#' @export
+geo_inventory_request <- function(credentials, 
+                                  report_type=c('Site', 
+                                                'Section'), 
+                                  report_geo=c('Executive Summary', 'Continent', 
+                                               'Country', 'State', 
+                                               'City', 'Postal Code', 
+                                               'Telephone Area Code', 
+                                               'DMA', 'MSA', 'Bandwidth'), 
+                                  report_outlook=c('Statistics', 'Forecast'),
+                                  id,
+                                  max_row="100",
+                                  position=NULL,
+                                  start_date=NULL,
+                                  end_date=NULL){
+  if(report_geo!='Executive Summary'){
+    which_report_row <- ((inventory_reports$inventory_type=='geo') & 
+                           (inventory_reports$report_type==tolower(report_type)) & 
+                           (inventory_reports$report_name==
+                              tolower(paste0(report_outlook, ' by ', report_geo))))
+  } else {
+    which_report_row <- ((inventory_reports$inventory_type=='geo') & 
+                           (inventory_reports$report_type==tolower(report_type)) & 
+                           (inventory_reports$report_name==tolower(report_geo)))
+  }
+  report_id <- inventory_reports[which_report_row, 'report_id']
+  if(length(report_id)!=1){
+    stop('report_id not found, please refer to dataset inventory_reports for correct report_type and report_name')
+  }
+  
+  adxml_node <- newXMLNode("AdXML")
+  request_node <- newXMLNode("Request", 
+                             attrs = c(type = "GeoInventory"), 
+                             parent = adxml_node)
+  report_node <- newXMLNode("GeoInventory", 
+                            attrs = c(type = report_type, maxRow=max_row), 
+                            parent = request_node)
+  id_node <- newXMLNode("Id", id, parent = report_node)
+  
+  if (!is.null(position)){
+    for (p in position){
+      position_node <- newXMLNode("Position", p, parent = report_node)
+    }
+  }
+  if (!is.null(start_date)){
+    start_date_node <- newXMLNode("StartDate", start_date, parent = report_node)
+  }
+  if (!is.null(end_date)){
+    end_date_node <- newXMLNode("EndDate", end_date, parent = report_node)
+  }
+  table_node <- newXMLNode("Table", report_id, parent = report_node)
+  
+  adxml_string <- as(adxml_node, "character")
+  
+  xmlBody <- request_builder(credentials=credentials, 
+                             adxml_request=adxml_string)
+  
+  result <- perform_request(xmlBody)
+  
+  parsed_result <- inventory_result_parser(result_text=result$text$value())
+  
+  return(parsed_result)
+}
+
+#' Retrieve Zone Inventory Reports
+#' 
+#' This function returns a data.frame of inventory stats 
+#' and forecasts broken down by a specified zone
+#'
+#' @usage zone_inventory_request(credentials, 
+#'                               report_name=c('Statistics by Zone', 
+#'                                             'Forecast by Zone', 
+#'                                             'Detail Statistics',
+#'                                             'Detail Forecast'), 
+#'                               site_id,
+#'                               max_row="100",
+#'                               zone_name=NULL,
+#'                               start_date=NULL,
+#'                               end_date=NULL)
+#' @concept api inventory zone report
+#' @include utils.R data.R
+#' @param credentials a character string as returned by \link{build_credentials}
+#' @param report_name a character string one of the valid zone inventory reports
+#' @param site_id a character string containing an identifying name for a Site
+#' @param max_row a character integer limiting the number of rows returned. 
+#' A character is recommended to avoid larger numbers being formatted
+#' in scientific notation and not being properly interpreted by the API.
+#' @param zone_name a character string of a particular zone name for reporting on. 
+#' @param start_date a character string for inquiring inventory detail report. 
+#' This field is optional and must be in the yyyy-mm-dd format.
+#' @param end_date a character string for inquiring the inventory detail report by 
+#' given schedule. This field is optional and must be in the yyyy-mm-dd format.
+#' @return A \code{data.frame} of zone inventory data in the format of the specified
+#' report_name
+#' @note Zone inventory requests are only available if the Custom Zip Zone module is enabled on the account
+#' @examples
+#' \dontrun{
+#' my_credentials <- build_credentials('myaccount', 
+#'                                     'myusername', 
+#'                                     'mypassword')
+#'                                     
+#' # Note that forecast reports must have start and end dates >= Sys.Date()
+#' otherwise they will return 0 row data.frames                                  
+#' zone_forecast <- zone_inventory_request(credentials=my_credentials, 
+#'                                         report_name='Forecast by Zone',
+#'                                         site_id='www.mysite.com',
+#'                                         max_row='20000',
+#'                                         zone_name='UNKNOWN'
+#'                                         start_date='2015-12-01', 
+#'                                         end_date='2015-12-31')
+#'                                      
+#' }
+#' @export
+zone_inventory_request <- function(credentials, 
+                                  report_name=c('Statistics by Zone', 
+                                                'Forecast by Zone', 
+                                                'Detail Statistics', 
+                                                'Detail Forecast'),
+                                  site_id,
+                                  max_row="100",
+                                  zone_name=NULL,
+                                  start_date=NULL,
+                                  end_date=NULL){
+
+  which_report_row <- ((inventory_reports$inventory_type=='zone') &
+                         (inventory_reports$report_name==tolower(report_name)))
+  
+  report_id <- inventory_reports[which_report_row, 'report_id']
+  if(length(report_id)!=1){
+    stop('report_id not found, please refer to dataset inventory_reports for correct report_type and report_name')
+  }
+  
+  adxml_node <- newXMLNode("AdXML")
+  request_node <- newXMLNode("Request", 
+                             attrs = c(type = "ZoneInventory"), 
+                             parent = adxml_node)
+  report_node <- newXMLNode("ZoneInventory", 
+                            attrs = c(type = "Site", maxRow=max_row), 
+                            parent = request_node)
+  id_node <- newXMLNode("Id", site_id, parent = report_node)
+  if (!is.null(zone_name)){
+    zone_node <- newXMLNode("ZoneName", zone_name, parent = report_node)
+  }
+  if (!is.null(start_date)){
+    start_date_node <- newXMLNode("StartDate", start_date, parent = report_node)
+  }
+  if (!is.null(end_date)){
+    end_date_node <- newXMLNode("EndDate", end_date, parent = report_node)
+  }
+  table_node <- newXMLNode("Table", report_id, parent = report_node)
+  
+  adxml_string <- as(adxml_node, "character")
+  
+  xmlBody <- request_builder(credentials=credentials, 
+                             adxml_request=adxml_string)
+  
+  result <- perform_request(xmlBody)
+  
+  parsed_result <- inventory_result_parser(result_text=result$text$value())
+  
+  return(parsed_result)
+}
 
 inventory_result_parser <- function(result_text){
   
--- README.md
+++ README.md
@@ -8,13 +8,17 @@ Interact with the Open Ad Stream (OAS) API from R
 
 Features:
 
+-   build\_credentials(): One-time create authentication credentials and re-use
 -   list\_request(): List OAS Objects into data.frame
 -   read\_request(): Read all fields on an OAS Object
 -   report\_request(): Run over 800 different template reports (Campaign Delivery, Account Revenue, etc.)
 -   basic\_inventory\_request(): Run inventory reports over 18 different types (Site, Section, Campaign, etc.)
 -   search\_inventory\_request(): Run inventory reports based on keyword search terms
+-   geo\_inventory\_request(): Run inventory on across different geography types (Country, State, Postal Code, etc.) for Sites and Sections
+-   zone\_inventory\_request(): Run zone inventory reports for a site
 
-### Install from Github using devtools
+Install from Github using devtools
+----------------------------------
 
 ``` r
 devtools::install_github('ReportMort/roas')
@@ -24,19 +28,44 @@ devtools::install_github('ReportMort/roas')
 library('roas')
 ```
 
-### Function naming convention
+Function naming convention
+--------------------------
 
 The functions are named to mimic each OAS request action ('Add', 'List', 'Update', 'Delete', 'Read', 'Copy', 'Upload', 'RunLive', 'Reports', 'Inventory'), but many of these actions are reserved words, so the functions are named {action}\_request(). For example, running the 'List' action is done with the function list\_request()
 
-### Common Usage
+Common Usage
+------------
 
-list\_request()
----------------
+### build\_credentials()
 
-List all sites in your account. Note that you first must build credentials for authorization and pass them to the request function. The credentials can be reused as many times as needed.
+Build credentials for authorization and pass them into subsequent request function calls. The credentials can be reused as many times as needed.
 
 ``` r
 my_credentials <- build_credentials('myaccount', 'myusername', 'mypassword')
+```
+
+### Setting options
+
+There is a set of 5 package options that are default set upon load. These may need to be configured based on your API instance. Set them using the `option()`
+
+``` r
+
+# defaults
+roas.url_endpoint = "https://openadstream18.247realmedia.com/oasapi/OaxApi",
+roas.namespace = "https://api.oas.tfsm.com/",
+roas.service_name = "OaxApiService",
+roas.port_name = "OaxApiPort",
+roas.method_name = "OasXmlRequest"
+
+# setting a new endpoint to use for requests
+options(roas.url_endpoint = "https://openadstream11.247realmedia.com/oasapi/OaxApi")
+```
+
+### list\_request()
+
+List all sites in your account.
+
+``` r
 list_of_sites <- list_request(credentials=my_credentials, request_type='Site')
 ```
 
@@ -59,8 +88,7 @@ list_w_criteria <- list_request(credentials=my_credentials, request_type='Page',
                                                                 '2013-12-31')))
 ```
 
-read\_request()
----------------
+### read\_request()
 
 Retrieve all available fields on a site and a particular campaign
 
@@ -73,8 +101,7 @@ campaign_details <- read_request(credentials=my_credentials,
                                  id='one_campaign_id')
 ```
 
-report\_request()
------------------
+### report\_request()
 
 Retrieve a template executive summary report on campaign delivery
 
@@ -86,8 +113,7 @@ campaign_delivery <- report_request(credentials=my_credentials,
                                     end_date='2015-09-30')
 ```
 
-basic\_inventory\_request()
----------------------------
+### basic\_inventory\_request()
 
 Retrieve an inventory forecast for all sites
 
@@ -102,7 +128,41 @@ overview <- basic_inventory_request(credentials=my_credentials,
                                     end_date='2015-12-31')
 ```
 
-### Overview of functions
+### search\_inventory\_request()
+
+Retrieve booked inventory based on two keywords (Kw1, Kw2) for a particular campaign on a site
+
+``` r
+
+booked <- search_inventory_request(credentials=my_credentials, 
+                                   report_type='KeywordBooked', 
+                                   report_name='Campaign Targets',
+                                   keywords='Kw1,Kw2',
+                                   campaign_id=c('Test_Campaign'),
+                                   site_domain=c('www.mysite.com'),
+                                   start_date='2015-12-01', 
+                                   end_date='2015-12-31')
+```
+
+### geo\_inventory\_request()
+
+Retrieve an inventory forecast for two positions on the "Books" section broken down by DMA
+
+``` r
+
+section_geo <- geo_inventory_request(credentials=my_credentials, 
+                                     report_type='Section', 
+                                     report_geo='DMA',
+                                     report_outlook='Forecast',
+                                     id='Books',
+                                     max_row="20000",
+                                     position=c('Bottom', 'Bottom1'),
+                                     start_date='2015-12-01', 
+                                     end_date='2015-12-31')
+```
+
+Overview of functions
+---------------------
 
 | function                   | description                                                      | status  |
 |:---------------------------|:-----------------------------------------------------------------|:--------|
@@ -112,7 +172,7 @@ overview <- basic_inventory_request(credentials=my_credentials,
 | read\_request              | Read details of a particular OAS instance                        | Done    |
 | report\_request            | Retrieve templatized OAS report                                  | Done    |
 | basic\_inventory\_request  | Run templatized reports on inventory                             | Done    |
-| search\_inventory\_request | Run templatized reports on inventory by search parameters        | TODO    |
-| geo\_inventory\_request    | Run templatized reports on inventory by geolocations             | TODO    |
-| zone\_inventory\_request   | Run templatized reports on inventory by zone                     | TODO    |
+| search\_inventory\_request | Run templatized reports on inventory by search parameters        | Done    |
+| geo\_inventory\_request    | Run templatized reports on inventory by geolocations             | Done    |
+| zone\_inventory\_request   | Run templatized reports on inventory by zone                     | Done    |
 | add\_request               | Add an instance of an OAS object (requires elevated permissions) | UNKNOWN |
